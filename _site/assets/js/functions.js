684cb399382de26f1b05ae3a6d41e349eb329e30
joseAntony <joseAntony@github.com>
2018-01-02 14:41:25 +0530

    smoothScroll(1000)

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -1,5 +1,5 @@
 $(function() {
-	smoothScroll(100000);
+	smoothScroll(1000);
 	$("#header h1").fitText(1, { minFontSize: '50px', maxFontSize: '170px' });
 	$("#header h2").fitText(1, { minFontSize: '20px', maxFontSize: '32px' });
 
